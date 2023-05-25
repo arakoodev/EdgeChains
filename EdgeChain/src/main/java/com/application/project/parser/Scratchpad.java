3f19c5587407afab1f233dd843348b01dce36494
MD SHAHIL <Shahilmd033@gmail.com>
2023-05-25 09:19:49 +0530

    Update Scratchpad.java

--- EdgeChain/src/main/java/com/application/project/parser/Scratchpad.java
+++ EdgeChain/src/main/java/com/application/project/parser/Scratchpad.java
@@ -43,6 +43,23 @@ public class Scratchpad {
     return actionContent;
   }
 
+
+  // method to extract the content between brackets for a given thought index
+public String getThoughtContent(int thoughtIndex) {
+  String thoughtContent = null;
+  for (String item : scratchpadList) {
+    if (item.startsWith("Thought " + thoughtIndex)) {
+      Pattern pattern = Pattern.compile("\\[(.*?)\\]");
+      Matcher matcher = pattern.matcher(item);
+      if (matcher.find()) {
+        thoughtContent = matcher.group(1);
+      }
+      break;
+    }
+  }
+  return thoughtContent;
+}
+
   // Method to replace the content of an action by the given index with a new string called
   // wikiContentForAction
   public void observationReplacer(String newString) {
@@ -67,52 +84,6 @@ public class Scratchpad {
     if (observationIndex >= 0 && observationIndex < scratchpadList.size() - 1) {
       scratchpadList.subList(observationIndex + 1, scratchpadList.size()).clear();
     }
-
-
-// method to extract the content between brackets for a given thought index
-public String getThoughtContent(int thoughtIndex) {
-  String thoughtContent = null;
-  for (String item : scratchpadList) {
-    if (item.startsWith("Thought " + thoughtIndex)) {
-      Pattern pattern = Pattern.compile("\\[(.*?)\\]");
-      Matcher matcher = pattern.matcher(item);
-      if (matcher.find()) {
-        thoughtContent = matcher.group(1);
-      }
-      break;
-    }
-  }
-  return thoughtContent;
-}
-
-
-// method to replace the content of a thought by the given index with a new string 
-public void thoughtReplacer(int thoughtIndex, String newThoughtContent) {
-  boolean thoughtFound = false;
-  int thoughtItemIndex = -1;
-  for (int i = 0; i < scratchpadList.size(); i++) {
-    String item = scratchpadList.get(i);
-    if (item.startsWith("Thought " + thoughtIndex)) {
-      String updatedItem = "Thought " + thoughtIndex + ": " + newThoughtContent;
-      scratchpadList.set(i, updatedItem);
-      thoughtFound = true;
-      thoughtItemIndex = i;
-      break;
-    }
-  }
-  if (!thoughtFound) {
-    scratchpadList.add("Thought " + thoughtIndex + ": " + newThoughtContent);
-    thoughtItemIndex = scratchpadList.size() - 1;
-  }
-
-  // Remove everything after the newly added or updated thought
-  if (thoughtItemIndex >= 0 && thoughtItemIndex < scratchpadList.size() - 1) {
-    scratchpadList.subList(thoughtItemIndex + 1, scratchpadList.size()).clear();
-  }
-}
-
-
-
 
   }
 }
