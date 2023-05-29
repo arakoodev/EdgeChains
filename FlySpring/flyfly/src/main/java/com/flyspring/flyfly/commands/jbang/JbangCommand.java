7aacfcde96c9cddcb0b6258fce257a94abb89221
igokhul <gokhul.tg@proton.me>
2023-05-29 17:14:56 +0530

    Automation for linux is added in JbangCommand.java

--- FlySpring/flyfly/src/main/java/com/flyspring/flyfly/commands/jbang/JbangCommand.java
+++ FlySpring/flyfly/src/main/java/com/flyspring/flyfly/commands/jbang/JbangCommand.java
@@ -46,32 +46,60 @@ public class JbangCommand implements Runnable {
 
   private void runJbang(File jarFile, String javaFile, String classPathJar) {
     try {
-      // Step One: Execute the initial command to get the classpath
-      ProcessBuilder pb = new ProcessBuilder(
-          "java",
-          "-cp",
-          jarFile.getAbsolutePath(),
-          "dev.jbang.Main",
-          "--cp",
-          classPathJar,
-          javaFile);
-      pb.redirectErrorStream(true);
-      Process process = pb.start();
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      String classPath = extractClassPathFromOutput(bufferedReader);
+      String os = System.getProperty("os.name").toLowerCase();
+      if (os.contains("win")) {
+        // Windows
+        // Step One: Execute the initial command to get the classpath
+        ProcessBuilder pb = new ProcessBuilder(
+            "java",
+            "-cp",
+            jarFile.getAbsolutePath(),
+            "dev.jbang.Main",
+            "--cp",
+            classPathJar,
+            javaFile);
+        pb.redirectErrorStream(true);
+        Process process = pb.start();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String classPath = extractClassPathFromOutput(bufferedReader);
 
-      // The mainClass accepts any class name provided from the javaFile
-      String mainClass = extractMainClassFromOutput(bufferedReader, javaFile);
-      System.out.println("Extracted Classpath: " + classPath);
-      System.out.println("Main Class: " + mainClass);
+        // The mainClass accepts any class name provided from the javaFile
+        String mainClass = extractMainClassFromOutput(bufferedReader, javaFile);
 
-      process.waitFor();
+        System.out.println("Extracted Classpath: " + classPath);
+        System.out.println("Main Class: " + mainClass);
 
-      // Step Two: Execute the final command with the extracted classpath
-      if (classPath != null && !classPath.isEmpty() && mainClass != null && !mainClass.isEmpty()) {
-        runJavaWithClassPath(classPath, mainClass);
+        process.waitFor();
+
+        // Step Two: Execute the final command with the extracted classpath
+        if (classPath != null && !classPath.isEmpty() && mainClass != null && !mainClass.isEmpty()) {
+          runJavaWithClassPath(classPath, mainClass);
+        } else {
+          System.out.println("Could not extract classpath or main class from the output.");
+        }
       } else {
-        System.out.println("Could not extract classpath or main class from the output.");
+        // Linux
+        // Step One: Execute the initial command to get the classpath
+        ProcessBuilder pb = new ProcessBuilder(
+            "java",
+            "-cp",
+            jarFile.getAbsolutePath(),
+            "dev.jbang.Main",
+            "--cp",
+            classPathJar,
+            javaFile);
+        pb.redirectErrorStream(true);
+        Process process = pb.start();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        
+        StringBuilder output = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+          output.append(line).append("\n");
+        }
+        process.waitFor();
+        String commandOutput = output.toString().trim();
+        System.out.println(commandOutput);
       }
     } catch (IOException | InterruptedException e) {
       e.printStackTrace();
