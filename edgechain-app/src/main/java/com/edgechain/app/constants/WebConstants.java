d76f297722c58af1d595a597e4ef9eee4f845dbf
Emad Hanif <132761026+EmadHanif@users.noreply.github.com>
2023-06-01 13:39:00 +0500

    Update WebConstants.java
    Implementation Microservice Architecture For EdgeChains

--- edgechain-app/src/main/java/com/edgechain/app/constants/WebConstants.java
+++ edgechain-app/src/main/java/com/edgechain/app/constants/WebConstants.java
@@ -2,6 +2,6 @@ package com.edgechain.app.constants;
 
 public class WebConstants {
 
-    public static final String OPENAI_AUTH_KEY = "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";
+    public static final String OPENAI_AUTH_KEY = "";
 
 }
