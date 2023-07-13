6068bc0d166a77d51edca657f19c1e805d4b2efa
EmadHanif <emadhanif200@gmail.com>
2023-07-14 02:30:16 +0500

    Update OpenAiClient

--- FlySpring/edgechain-app/src/test/java/com/edgechain/openai/OpenAiClientTest.java
+++ FlySpring/edgechain-app/src/test/java/com/edgechain/openai/OpenAiClientTest.java
@@ -3,36 +3,43 @@ package com.edgechain.openai;
 import com.edgechain.lib.embeddings.WordEmbeddings;
 import com.edgechain.lib.endpoint.impl.OpenAiEndpoint;
 import com.edgechain.lib.openai.response.ChatCompletionResponse;
-import com.edgechain.lib.rxjava.retry.impl.ExponentialDelay;
-;
+import com.edgechain.lib.rxjava.retry.impl.ExponentialDelay;;
+import com.edgechain.lib.rxjava.retry.impl.FixedDelay;
 import io.reactivex.rxjava3.observers.TestObserver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.concurrent.TimeUnit;
 
 import static com.edgechain.lib.constants.EndpointConstants.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 public class OpenAiClientTest {
 
-  @LocalServerPort int randomServerPort;
+  @LocalServerPort
+  int randomServerPort;
 
   private Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @BeforeEach
   public void setup() {
-    System.setProperty("server.port", "" + randomServerPort);
+    System.setProperty("server.port", ""+randomServerPort);
   }
 
-  @ParameterizedTest
+
+    @ParameterizedTest
   @CsvSource({
     "Write 10 unique sentences on Java Language",
     "Can you explain Ant Bee Colony Optimization Algorithm?"
@@ -50,7 +57,7 @@ public class OpenAiClientTest {
             "user",
             0.7,
             false,
-            new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
+            new FixedDelay(2,3,TimeUnit.SECONDS));
 
     TestObserver<ChatCompletionResponse> test = endpoint.getChatCompletion(prompt).test();
 
@@ -61,38 +68,7 @@ public class OpenAiClientTest {
 
     // Assert
     test.assertNoErrors();
-  }
 
-  @ParameterizedTest
-  @CsvSource({
-    "Write 10 unique sentences on Java Language",
-    "Can you explain Ant Bee Colony Optimization Algorithm?"
-  })
-  @DisplayName("Test OpenAI ChatCompletion Stream")
-  public void testOpenAiEndpoint_ChatCompletionStreamResponseShouldAssertNoErrors(String prompt)
-      throws InterruptedException {
-
-    // Step 1 : Create OpenAi Endpoint
-    OpenAiEndpoint endpoint =
-        new OpenAiEndpoint(
-            OPENAI_CHAT_COMPLETION_API,
-            "", // apiKey
-            "", // orgId
-            "gpt-3.5-turbo",
-            "user",
-            0.7,
-            true,
-            new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
-
-    TestObserver<ChatCompletionResponse> test = endpoint.getChatCompletion(prompt).test();
-
-    // Step 4: To act & assert
-    test.await();
-
-    logger.info(test.values().toString());
-
-    // Assert
-    test.assertNoErrors();
   }
 
   @Test
@@ -103,15 +79,15 @@ public class OpenAiClientTest {
 
     // Step 1 : Create OpenAi Endpoint
     OpenAiEndpoint endpoint =
-        new OpenAiEndpoint(
-            OPENAI_EMBEDDINGS_API,
-            "", // apiKey
-            "", // orgId
-            "text-embedding-ada-002", // model
-            null,
-            null,
-            null,
-            new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
+            new OpenAiEndpoint(
+                    OPENAI_EMBEDDINGS_API,
+                    "", // apiKey
+                    "", // orgId
+                    "text-embedding-ada-002", // model
+                    null,
+                    null,
+                    null,
+                    new FixedDelay(2,3,TimeUnit.SECONDS));
 
     TestObserver<WordEmbeddings> test = endpoint.getEmbeddings(input).test();
 
