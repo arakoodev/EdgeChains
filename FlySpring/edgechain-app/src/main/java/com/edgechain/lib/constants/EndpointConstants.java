360d8a76217eb0ca0974a8c10b55eb35dfeb959c
Supriya Ghorai <78735395+soumitra2001@users.noreply.github.com>
2023-07-13 17:37:26 +0530

    Adding Test Cases (#138)
    * Google Java Format
    
    * Google Java Format
    
    * Google Java Format
    
    * Adding Test Cases
    
    * Adding Test Cases
    
    * Adding Test Cases || Modified RedisClientTest.java
    
    * Update OpenAiClientTest.java

--- FlySpring/edgechain-app/src/main/java/com/edgechain/lib/constants/EndpointConstants.java
+++ FlySpring/edgechain-app/src/main/java/com/edgechain/lib/constants/EndpointConstants.java
@@ -9,6 +9,4 @@ public class EndpointConstants {
 
   public static final String OPENAI_EMBEDDINGS_API = "https://api.openai.com/v1/embeddings";
 
-  public static final String OPENAI_COMPLETION_API = "https://api.openai.com/v1/completions";
-
 }
--- FlySpring/edgechain-app/src/test/java/com/edgechain/openai/OpenAiClientTest.java
+++ FlySpring/edgechain-app/src/test/java/com/edgechain/openai/OpenAiClientTest.java
@@ -9,18 +9,28 @@ import com.edgechain.lib.openai.request.CompletionRequest;
 import com.edgechain.lib.openai.response.ChatCompletionResponse;
 import com.edgechain.lib.openai.response.CompletionResponse;
 import com.edgechain.lib.rxjava.retry.impl.ExponentialDelay;
+import com.edgechain.lib.rxjava.retry.impl.FixedDelay;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.reactivex.rxjava3.observers.TestObserver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.edgechain.lib.constants.EndpointConstants.*;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class OpenAiClientTest {
@@ -34,24 +44,24 @@ public class OpenAiClientTest {
 
   @ParameterizedTest
   @CsvSource({
-    "Write 10 unique sentences on Java Language",
-    "Can you explain Ant Bee Colony Optimization Algorithm?"
+          "Write 10 unique sentences on Java Language",
+          "Can you explain Ant Bee Colony Optimization Algorithm?"
   })
   @DisplayName("Test OpenAI ChatCompletion")
   public void testOpenAiEndpoint_ChatCompletionShouldAssertNoErrors(String prompt)
-      throws InterruptedException {
+          throws InterruptedException {
 
     // Step 1 : Create OpenAi Endpoint
     OpenAiEndpoint endpoint =
-        new OpenAiEndpoint(
-            OPENAI_CHAT_COMPLETION_API,
-            "", // apiKey
-            "", // orgId
-            "gpt-3.5-turbo",
-            "user",
-            0.7,
-            false,
-            new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
+            new OpenAiEndpoint(
+                    OPENAI_CHAT_COMPLETION_API,
+                    "", // apiKey
+                    "", // orgId
+                    "gpt-3.5-turbo",
+                    "user",
+                    0.7,
+                    false,
+                    new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
 
     TestObserver<ChatCompletionResponse> test = endpoint.getChatCompletion(prompt).test();
 
@@ -66,26 +76,55 @@ public class OpenAiClientTest {
 
   @ParameterizedTest
   @CsvSource({
-    "Write 10 unique sentences on Java Language",
-    "Can you explain Ant Bee Colony Optimization Algorithm?"
+          "Write 10 unique sentences on Java Language",
+          "Can you explain Ant Bee Colony Optimization Algorithm?"
   })
   @DisplayName("Test OpenAI ChatCompletion Stream")
   public void testOpenAiEndpoint_ChatCompletionStreamResponseShouldAssertNoErrors(String prompt)
-      throws InterruptedException {
+          throws InterruptedException {
 
     // Step 1 : Create OpenAi Endpoint
     OpenAiEndpoint endpoint =
-        new OpenAiEndpoint(
-            OPENAI_CHAT_COMPLETION_API,
-            "", // apiKey
-            "", // orgId
-            "gpt-3.5-turbo",
-            "user",
-            0.7,
-            true,
-            new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
+            new OpenAiEndpoint(
+                    OPENAI_CHAT_COMPLETION_API,
+                    "", // apiKey
+                    "", // orgId
+                    "gpt-3.5-turbo",
+                    "user",
+                    0.7,
+                    true,
+                    new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
 
     TestObserver<ChatCompletionResponse> test = endpoint.getChatCompletion(prompt).test();
+
+    // Step 4: To act & assert
+    test.await();
+
+    logger.info(test.values().toString());
+
+    // Assert
+    test.assertNoErrors();
+  }
+
+  @Test
+  @DisplayName("Test OpenAI Embeddings")
+  public void testOpenAiEndpoint_EmbeddingsShouldAssertNoErrors() throws InterruptedException {
+
+    String input = "Hey, we are building LLMs using Spring and Java";
+
+    // Step 1 : Create OpenAi Endpoint
+    OpenAiEndpoint endpoint =
+            new OpenAiEndpoint(
+                    OPENAI_EMBEDDINGS_API,
+                    "", // apiKey
+                    "", // orgId
+                    "text-embedding-ada-002", // model
+                    null,
+                    null,
+                    null,
+                    new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
+
+    TestObserver<WordEmbeddings> test = endpoint.getEmbeddings(input).test();
 
     // Step 4: To act & assert
     test.await();
@@ -108,6 +147,40 @@ public class OpenAiClientTest {
     );
 
     String prompt = "Write an unique jokes";
+
+    //Step 2: Create a CompletionRequest for createCompletion method
+    CompletionRequest completionRequest= CompletionRequest.builder()
+            .model(endpoint.getModel())
+            .prompt(prompt)
+            .build();
+
+    // Step 3: OpenAiClient
+    TestObserver<CompletionResponse> test = new OpenAiClient(endpoint).createCompletion(completionRequest)
+            .getScheduledObservable().test();
+
+    // Step 4: To act & assert
+    test.await();
+
+    logger.info(test.values().toString());
+
+    // Assert
+    test.assertNoErrors();
+
+  }
+
+  @Test
+  @DisplayName("OpenAiClient Create_Completion Test2")
+  public void test2_OpenAiClient_Completion() throws InterruptedException {
+
+    //Step 1: Create OpenAi endpoint
+    OpenAiEndpoint endpoint = new OpenAiEndpoint(
+            OPENAI_COMPLETION_API,
+            "",
+            "text-davinci-003",
+            new FixedDelay(3,3,TimeUnit.SECONDS)
+    );
+
+    String prompt = "The function g is defined by g(x)=(x-3)^2. If g(a)=a^2, what is the value of a?";
 
     //Step 2: Create a CompletionRequest for createCompletion method
     CompletionRequest completionRequest= CompletionRequest.builder()
@@ -161,25 +234,6 @@ public class OpenAiClientTest {
                     .getScheduledObservable()
                     .test();
 
-  @DisplayName("Test OpenAI Embeddings")
-  public void testOpenAiEndpoint_EmbeddingsShouldAssertNoErrors() throws InterruptedException {
-
-    String input = "Hey, we are building LLMs using Spring and Java";
-
-    // Step 1 : Create OpenAi Endpoint
-    OpenAiEndpoint endpoint =
-        new OpenAiEndpoint(
-            OPENAI_EMBEDDINGS_API,
-            "", // apiKey
-            "", // orgId
-            "text-embedding-ada-002", // model
-            null,
-            null,
-            null,
-            new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
-
-    TestObserver<WordEmbeddings> test = endpoint.getEmbeddings(input).test();
-
     // Step 4: To act & assert
     test.await();
 
@@ -187,40 +241,6 @@ public class OpenAiClientTest {
 
     // Assert
     test.assertNoErrors();
-  }
-
-  @Test
-  @DisplayName("OpenAiClient Create_Completion Test2")
-  public void test2_OpenAiClient_Completion() throws InterruptedException {
-
-    //Step 1: Create OpenAi endpoint
-    OpenAiEndpoint endpoint = new OpenAiEndpoint(
-            OPENAI_COMPLETION_API,
-            "",
-            "text-davinci-003",
-            new FixedDelay(3,3,TimeUnit.SECONDS)
-    );
-
-    String prompt = "The function g is defined by g(x)=(x-3)^2. If g(a)=a^2, what is the value of a?";
-
-    //Step 2: Create a CompletionRequest for createCompletion method
-    CompletionRequest completionRequest= CompletionRequest.builder()
-            .model(endpoint.getModel())
-            .prompt(prompt)
-            .build();
-
-    // Step 3: OpenAiClient
-    TestObserver<CompletionResponse> test = new OpenAiClient(endpoint).createCompletion(completionRequest)
-            .getScheduledObservable().test();
-
-    // Step 4: To act & assert
-    test.await();
-
-    logger.info(test.values().toString());
-
-    // Assert
-    test.assertNoErrors();
-
   }
 
 }
