566bb0799661ed6390716ac7788c0a9c6c8f5c40
Prakash A <prakashaathi321@gmail.com>
2023-07-23 11:07:33 +0530

    Added test cases

---
+++ FlySpring/edgechain-app/src/test/java/com/edgechain/chunker/ChunkerTest.java
@@ -0,0 +1,94 @@
+package com.edgechain.chunker;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import com.edgechain.lib.chunk.Chunker;
+import com.edgechain.lib.chunk.enums.LangType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ChunkerTest {
+
+    @LocalServerPort
+    int randomServerPort;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("server.port", "" + randomServerPort);
+    }
+
+    @Test
+    @DisplayName("Test By Chunk Size")
+    public void chunker_ByChunkSize_ReturnExpectedValue(TestInfo testInfo) throws Exception {
+
+        logger.info("======== " + testInfo.getDisplayName() + " ========");
+
+        String input = "This is a test string for chunking.";
+        Chunker chunker = new Chunker(input);
+        int chunkSize = 5;
+
+        String[] result = chunker.byChunkSize(chunkSize);
+
+        String[] expected = { "This", "is a", "test", "strin", "g for", "chun", "king." };
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Test By Sentence")
+    public void chunker_BySentence_ReturnExpectedValue(TestInfo testInfo) throws Exception {
+
+        logger.info("======== " + testInfo.getDisplayName() + " ========");
+
+        String input = "This is sentence one. Sentence two. Sentence three.";
+        Chunker chunker = new Chunker(input);
+        LangType langType = LangType.EN;
+
+        String[] result = chunker.bySentence(langType);
+
+        String[] expected = { "This is sentence one.", "Sentence two.", "Sentence three." };
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Test By Chunk Size With Empty Input ")
+    public void chunker_ByChunkSizeWithEmptyInput_ReturnEmptyArray(TestInfo testInfo) throws Exception {
+
+        logger.info("======== " + testInfo.getDisplayName() + " ========");
+
+        String input = "";
+        Chunker chunker = new Chunker(input);
+        int chunkSize = 5;
+
+        String[] result = chunker.byChunkSize(chunkSize);
+
+        String[] expected = {};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Test By Sentence With Empty Input ")
+    public void chunker_BySentenceWithEmptyInput_ReturnEmptyArray(TestInfo testInfo) throws Exception {
+
+        logger.info("======== " + testInfo.getDisplayName() + " ========");
+
+        String input = "This is a test.";
+        Chunker chunker = new Chunker(input);
+        int chunkSize = 100;
+
+        String[] result = chunker.byChunkSize(chunkSize);
+
+        String[] expected = { "This is a test." };
+        assertArrayEquals(expected, result);
+    }
+
+}
---
+++ FlySpring/edgechain-app/src/test/java/com/edgechain/wiki/WikiControllerTest.java
@@ -0,0 +1,82 @@
+package com.edgechain.wiki;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import com.edgechain.lib.endpoint.impl.WikiEndpoint;
+import com.edgechain.lib.wiki.response.WikiResponse;
+import com.edgechain.service.controllers.wiki.WikiController;
+
+import io.reactivex.rxjava3.core.Single;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class WikiControllerTest {
+
+    @LocalServerPort
+    int randomServerPort;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("server.port", "" + randomServerPort);
+    }
+
+    @Autowired
+    private WikiController wikiController;
+
+    @Test
+    @DisplayName("Test WikiContent Method Returns WikiResponse")
+    @Order(1)
+    public void wikiControllerTest_TestWikiContentMethod_ReturnsWikiResponse(TestInfo testInfo) {
+
+        logger.info("======== " + testInfo.getDisplayName() + " ========");
+
+        // Prepare test data
+        WikiEndpoint wikiEndpoint = new WikiEndpoint();
+        wikiEndpoint.setInput("what is the Redis?");
+
+        // Call the wikiContent method
+        Single<WikiResponse> result = wikiController.wikiContent(wikiEndpoint);
+
+        // Verify the response
+        assertNotNull(result);
+        assertEquals(WikiResponse.class, result.blockingGet().getClass());
+    }
+
+    @Test
+    @DisplayName("Test WikiContent Method Populates Response Text")
+    @Order(2)
+    public void wikiControllerTest_TestWikiContentMethodPopulates_ReturnResponseText(TestInfo testInfo) {
+
+        logger.info("======== " + testInfo.getDisplayName() + " ========");
+
+        // Prepare test data
+        WikiEndpoint wikiEndpoint = new WikiEndpoint();
+        wikiEndpoint.setInput("what is the Redis?");
+
+        // Call the wikiContent method
+        Single<WikiResponse> result = wikiController.wikiContent(wikiEndpoint);
+
+        // Verify that the response text is populated
+        assertNotNull(result);
+        WikiResponse response = result.blockingGet();
+        assertNotNull(response);
+        assertNotNull(response.getText());
+    }
+
+}
