c413fb7eb048ee26e804cc0632ff7e26e45b6401
Arth <arth4701@gmail.com>
2023-07-31 19:17:48 +0530

    Xtrasonnet Custom Function (#148)
    * custom function in sjsonnet
    
    * hardcoded api call to wikipedia
    
    * xtrasonnet custom function for search added by creating a udf namespace
    
    * wrote a custom search function with wiki api and used that in jsonnet, wrote some logic for react chain
    
    * code cleanup and conflict resolved
    
    * wrote logic for react chain, some tests for react-chain and xtrasonnet, fixed other jsonnet files
    
    * fixed HistoryContext merge issue
    
    * removed old react-chain.jsonnet
    
    * ReactChainApplication refactoring, null and empty string check for xtrasonnet custom function
    
    * std.extVar to payload
    
    * removed sample function

---
+++ FlySpring/edgechain-app/src/main/java/com/edgechain/lib/jsonnet/XtraSonnetCustomFunc.java
@@ -0,0 +1,36 @@
+package com.edgechain.lib.jsonnet;
+
+import com.edgechain.lib.endpoint.impl.WikiEndpoint;
+import com.edgechain.lib.wiki.response.WikiResponse;
+import io.github.jam01.xtrasonnet.DataFormatService;
+import io.github.jam01.xtrasonnet.header.Header;
+import io.github.jam01.xtrasonnet.spi.Library;
+import io.reactivex.rxjava3.core.Observable;
+import sjsonnet.Importer;
+import sjsonnet.Val;
+
+import java.util.*;
+
+public class XtraSonnetCustomFunc extends Library {
+    @Override
+    public String namespace() {
+        return "udf";
+    }
+
+    @Override
+    public Map<String, Val.Func> functions(DataFormatService dataFormats, Header header, Importer importer) {
+        var res = new HashMap<String, Val.Func>();
+        res.put("fn", builtin(new String[]{"param"}, (vals, pos, ev) -> {
+            String prompt = vals[0].asString();
+            if(prompt == null || prompt.equals("")) {
+                return new Val.Str(dummyPosition(), "");
+            }
+            WikiEndpoint wikiEndpoint = new WikiEndpoint();
+
+            Observable<WikiResponse> result = wikiEndpoint.getPageContent(prompt);
+            String response = result.blockingFirst().getText();
+            return new Val.Str(dummyPosition(), response);
+        }));
+        return res;
+    }
+}
