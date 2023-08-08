0978096c396e7309c0a0c2ec7b413a4991059f28
Emad Hanif <132761026+EmadHanif@users.noreply.github.com>
2023-08-08 14:15:57 +0500

    Fixing Jbang Issue (#186)

--- FlySpring/edgechain-app/src/main/java/com/edgechain/lib/flyfly/commands/jbang/JbangResponse.java
+++ FlySpring/edgechain-app/src/main/java/com/edgechain/lib/flyfly/commands/jbang/JbangResponse.java
@@ -1,7 +1,9 @@
 package com.edgechain.lib.flyfly.commands.jbang;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JbangResponse {
   private String originalResource;
   private String backingResource;
