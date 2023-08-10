d286fdb90f7ea34d0972e7fbf5031868ecb1d0aa
Prakash A <prakashaathi321@gmail.com>
2023-08-10 19:08:39 +0530

    added code Interpreter

--- Examples/code-interpreter/CodeInterpreter.java
+++ Examples/code-interpreter/CodeInterpreter.java
@@ -1,27 +1,36 @@
 package com.edgechain;
 
+import static com.edgechain.lib.constants.EndpointConstants.OPENAI_CHAT_COMPLETION_API;
+
 import java.util.concurrent.TimeUnit;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edgechain.lib.codeInterpreter.Eval;
 import com.edgechain.lib.endpoint.impl.OpenAiEndpoint;
+import com.edgechain.lib.jsonnet.JsonnetArgs;
+import com.edgechain.lib.jsonnet.JsonnetLoader;
+import com.edgechain.lib.jsonnet.enums.DataType;
+import com.edgechain.lib.jsonnet.impl.FileJsonnetLoader;
 import com.edgechain.lib.request.ArkRequest;
 import com.edgechain.lib.rxjava.retry.impl.ExponentialDelay;
-
-import static com.edgechain.lib.constants.EndpointConstants.OPENAI_CHAT_COMPLETION_API;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class CodeInterpreter {
 
     private static final String OPENAI_AUTH_KEY = "";
     private static OpenAiEndpoint userChatEndpoint;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static JsonnetLoader loader = new FileJsonnetLoader("./code-interpreter/code-interpreter.jsonnet");
 
     public static void main(String[] args) {
         System.setProperty("server.port", "8080");
@@ -29,10 +38,10 @@ public class CodeInterpreter {
     }
 
     @RestController
-    @RequestMapping()
+    @RequestMapping("/v1/examples")
     public class interpreter {
 
-        @GetMapping("/")
+        @PostMapping("/code-interpreter")
         public double interpret(ArkRequest arkRequest) throws JSONException {
 
             JSONObject json = arkRequest.getBody();
@@ -45,18 +54,43 @@ public class CodeInterpreter {
                     0.7,
                     new ExponentialDelay(3, 5, 2, TimeUnit.SECONDS));
 
-            String response = userChatEndpoint
-                    .chatCompletion(json.getString("code"), "Json-format", arkRequest)
-                    .blockingFirst()
-                    .getChoices()
-                    .get(0)
-                    .getMessage()
-                    .getContent();
+            loader
+                    .put("prompt", new JsonnetArgs(DataType.STRING, json.getString("prompt")))
+                    .loadOrReload();
 
-            System.out.println(response + " from chatgpt3");
-            System.out.println(Eval.evaluateExpression(json.getString("code")) + " from Eval");
+            String prompt = loader.get("extract");
 
-            return Eval.evaluateExpression(json.getString("code"));
+            while (true) {
+
+                String response = userChatEndpoint
+                        .chatCompletion(prompt, "code-interpreter", arkRequest)
+                        .blockingFirst()
+                        .getChoices()
+                        .get(0)
+                        .getMessage()
+                        .getContent();
+
+                try {
+                    JsonNode jsonNode = objectMapper.readTree(response);
+                    System.out.println("Valid JSON from chatgpt " + jsonNode);
+
+                    String tool = jsonNode.get("action").get("tool").asText();
+                    String arg = jsonNode.get("action").get("arg").asText();
+
+                    if ("CodeInterpreter".equals(tool)) {
+                        Double val = Eval.evaluateExpression(arg);
+                        System.out.println("CodeInterpreter: " + val);
+
+                        prompt += String.format("\n%s\nObservation: CodeInterpreter returned %s", response, val);
+                    } else if ("Finish".equals(tool)) {
+                        System.out.println("FINAL ANSWER: " + arg);
+                        return Double.parseDouble(arg);
+                    }
+                } catch (JsonProcessingException e) {
+                    System.out.println("Invalid JSON from chatgpt " + response);
+                    e.printStackTrace();
+                }
+            }
 
         }
 
