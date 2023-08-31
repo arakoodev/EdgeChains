3e52a1d2395d27e6f8d8a77fe868759371cca765
Robin Ellis <robin.cake.ellis@gmail.com>
2023-08-31 11:25:41 +0700

    Use logger template {} instead of string concats
    - plus test

---
+++ FlySpring/edgechain-app/src/test/java/com/edgechain/EdgeChainApplicationTest.java
@@ -0,0 +1,14 @@
+package com.edgechain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeChainApplicationTest {
+
+  @Test
+  void test() {
+    // simply checks server can start
+  }
+
+}
