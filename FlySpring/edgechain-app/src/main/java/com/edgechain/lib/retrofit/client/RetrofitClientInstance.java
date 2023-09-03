2110ef249ae118a116583784d5286b0d1a0361c2
Robin Ellis <30558735+robincakeellis@users.noreply.github.com>
2023-09-03 13:45:14 +0700

    Fix stall issue with Retrofit OKHttp interceptor (#210)
    - noticed builds would hang for six hours on Github
    - due to test breaking interceptor with non IOException
    - fix interceptor with a try..catch
    - test for interceptor
    - reinstate BgeSmallEndpoint test
    - local build + jar run with test java completed ok

--- FlySpring/edgechain-app/src/main/java/com/edgechain/lib/retrofit/client/RetrofitClientInstance.java
+++ FlySpring/edgechain-app/src/main/java/com/edgechain/lib/retrofit/client/RetrofitClientInstance.java
@@ -13,27 +13,38 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import okhttp3.*;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
-import retrofit2.converter.jackson.JacksonConverterFactory;
-
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class RetrofitClientInstance {
+  
+  private RetrofitClientInstance() {
+    // no
+  }
 
   private static final String BASE_URL = "http://0.0.0.0";
 
-  private static SecurityUUID securityUUID =
-      ApplicationContextHolder.getContext().getBean(SecurityUUID.class);
-
+  private static SecurityUUID securityUUID;
   private static Retrofit retrofit;
 
   public static Retrofit getInstance() {
     if (retrofit == null) {
+      
+      // tests may set this to a mock - do not overwrite it if present.
+      if (securityUUID == null) {
+        securityUUID = ApplicationContextHolder.getContext().getBean(SecurityUUID.class);
+      }
 
       return retrofit =
           new Retrofit.Builder()
@@ -43,44 +54,52 @@ public class RetrofitClientInstance {
                       + System.getProperty("server.port")
                       + WebConfiguration.CONTEXT_PATH
                       + "/")
-              .addConverterFactory(JacksonBuilder())
+              .addConverterFactory(createJacksonFactory())
               .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
               .client(
                   new OkHttpClient.Builder()
                       .connectionPool(new ConnectionPool(10, 5, TimeUnit.MINUTES))
                       .addInterceptor(
                           chain -> {
-                            Request original = chain.request();
-                            Request request =
-                                original
-                                    .newBuilder()
-                                    .header("Authorization", securityUUID.getAuthKey())
-                                    .build();
-                            Response response = chain.proceed(request);
-                            String body = response.body().string();
-
-                            String errorMessage = "";
-
-                            if (!response.isSuccessful()) {
-                              // Create a new Gson object
-                              Gson gson = new Gson();
-
-                              // Define the type for the map
-                              Type type = new TypeToken<Map<String, String>>() {}.getType();
-
-                              // Convert JSON string into a map
-                              Map<String, String> map = gson.fromJson(body, type);
-
-                              if (Objects.nonNull(map)) {
-                                errorMessage = map.toString();
+                            try {
+                              Request original = chain.request();
+                              final String authKey = securityUUID.getAuthKey();
+                              Request request =
+                                  original
+                                      .newBuilder()
+                                      .header("Authorization", authKey)
+                                      .build();
+                              Response response = chain.proceed(request);
+                              String body = response.body().string();
+  
+                              String errorMessage = "";
+  
+                              if (!response.isSuccessful()) {
+                                // Create a new Gson object
+                                Gson gson = new Gson();
+  
+                                // Define the type for the map
+                                Type type = new TypeToken<Map<String, String>>() {}.getType();
+  
+                                // Convert JSON string into a map
+                                Map<String, String> map = gson.fromJson(body, type);
+  
+                                if (Objects.nonNull(map)) {
+                                  errorMessage = map.toString();
+                                }
                               }
+  
+                              return response
+                                  .newBuilder()
+                                  .body(ResponseBody.create(body, response.body().contentType()))
+                                  .message(errorMessage)
+                                  .build();
+                            }catch(Exception e) {
+                              // Interceptor can handle only IOException. Anything else = stall.
+                              // Solution: wrap any exception in an IOException.
+                              // Read more here: https://github.com/square/retrofit/issues/3453
+                              throw new IOException(e);
                             }
-
-                            return response
-                                .newBuilder()
-                                .body(ResponseBody.create(body, response.body().contentType()))
-                                .message(errorMessage)
-                                .build();
                           })
                       .connectTimeout(15, TimeUnit.MINUTES)
                       .readTimeout(20, TimeUnit.MINUTES)
@@ -90,7 +109,7 @@ public class RetrofitClientInstance {
     return retrofit;
   }
 
-  private static JacksonConverterFactory JacksonBuilder() {
+  private static JacksonConverterFactory createJacksonFactory() {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     objectMapper.registerModule(new JavaTimeModule());
