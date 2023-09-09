2007ddb58b3d173bab1b8d90f68c377b28e3bdc3
hemantDwivedi <hemantdwiwedi@gmail.com>
2023-09-09 20:02:52 +0530

    Adding annotation for sql script

--- FlySpring/edgechain-app/src/main/resources/schema.sql
+++ FlySpring/edgechain-app/src/main/resources/schema.sql
@@ -1,5 +1,6 @@
-CREATE TABLE history_context (
-                               id VARCHAR(255) NOT NULL PRIMARY KEY,
-                               response VARCHAR(1024),
-                               created_at TIMESTAMP
+CREATE TABLE history_context
+(
+    id         VARCHAR(255) NOT NULL PRIMARY KEY,
+    response   VARCHAR(1024),
+    created_at TIMESTAMP
 );
