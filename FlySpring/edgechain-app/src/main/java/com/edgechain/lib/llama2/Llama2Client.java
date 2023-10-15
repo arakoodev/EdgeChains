24cf405103d51bfde18f1cb86bde00c1b85d78b7
Hemant <hemantdwiwedi@gmail.com>
2023-10-15 20:38:32 +0530

    Get request (#255)
    * Adding support for LLama API
    
    * Adding support for GET request
    
    * Refactoring llama endpoint

--- FlySpring/edgechain-app/src/main/java/com/edgechain/lib/llama2/Llama2Client.java
+++ FlySpring/edgechain-app/src/main/java/com/edgechain/lib/llama2/Llama2Client.java
@@ -1,5 +1,6 @@
 package com.edgechain.lib.llama2;
 
+import com.edgechain.lib.endpoint.impl.llm.LLamaQuickstart;
 import com.edgechain.lib.endpoint.impl.llm.Llama2Endpoint;
 import com.edgechain.lib.llama2.request.Llama2ChatCompletionRequest;
 import com.edgechain.lib.llama2.response.Llama2ChatCompletionResponse;
@@ -14,51 +15,77 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class Llama2Client {
-  @Autowired private ObjectMapper objectMapper;
-  private final Logger logger = LoggerFactory.getLogger(getClass());
-  private final RestTemplate restTemplate = new RestTemplate();
+    @Autowired
+    private ObjectMapper objectMapper;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final RestTemplate restTemplate = new RestTemplate();
 
-  public EdgeChain<List<Llama2ChatCompletionResponse>> createChatCompletion(
-      Llama2ChatCompletionRequest request, Llama2Endpoint endpoint) {
-    return new EdgeChain<>(
-        Observable.create(
-            emitter -> {
-              try {
+    public EdgeChain<List<Llama2ChatCompletionResponse>> createChatCompletion(
+            Llama2ChatCompletionRequest request, Llama2Endpoint endpoint) {
+        return new EdgeChain<>(
+                Observable.create(
+                        emitter -> {
+                            try {
 
-                logger.info("Logging ChatCompletion....");
+                                logger.info("Logging ChatCompletion....");
 
-                logger.info("==============REQUEST DATA================");
-                logger.info(request.toString());
+                                logger.info("==============REQUEST DATA================");
+                                logger.info(request.toString());
 
-                //                                Llama2ChatCompletionRequest llamaRequest = new
-                // Llama2ChatCompletionRequest();
-                //
-                //                                llamaRequest.setInputs(request.getInputs());
-                //
-                // llamaRequest.setParameters(request.getParameters());
+                                //  Create headers
+                                HttpHeaders headers = new HttpHeaders();
+                                headers.setContentType(MediaType.APPLICATION_JSON);
+                                HttpEntity<Llama2ChatCompletionRequest> entity = new HttpEntity<>(request, headers);
+                                //
+                                String response =
+                                        restTemplate.postForObject(endpoint.getUrl(), entity, String.class);
 
-                //  Create headers
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.APPLICATION_JSON);
-                HttpEntity<Llama2ChatCompletionRequest> entity = new HttpEntity<>(request, headers);
-                //
-                String response =
-                    restTemplate.postForObject(endpoint.getUrl(), entity, String.class);
+                                List<Llama2ChatCompletionResponse> chatCompletionResponse =
+                                        objectMapper.readValue(
+                                                response, new TypeReference<>() {
+                                                });
+                                emitter.onNext(chatCompletionResponse);
+                                emitter.onComplete();
 
-                List<Llama2ChatCompletionResponse> chatCompletionResponse =
-                    objectMapper.readValue(
-                        response, new TypeReference<List<Llama2ChatCompletionResponse>>() {});
-                emitter.onNext(chatCompletionResponse);
-                emitter.onComplete();
+                            } catch (final Exception e) {
+                                emitter.onError(e);
+                            }
+                        }),
+                endpoint);
+    }
 
-              } catch (final Exception e) {
-                emitter.onError(e);
-              }
-            }),
-        endpoint);
-  }
+    public EdgeChain<String> createGetChatCompletion(LLamaQuickstart endpoint) {
+        return new EdgeChain<>(
+                Observable.create(
+                        emitter -> {
+                            try {
+
+                                //  Create headers
+                                HttpHeaders headers = new HttpHeaders();
+                                headers.set("User-Agent", "insomnia/8.2.0");
+                                HttpEntity<?> entity = new HttpEntity<>(headers);
+
+                                Map<String, String> param = Collections.singletonMap("query", endpoint.getQuery());
+
+                                String endpointUrl = endpoint.getUrl() + "?query={query}";
+
+                                ResponseEntity<String> response = restTemplate.exchange(endpointUrl, HttpMethod.GET, entity, String.class, param);
+
+                                logger.info("\nRESPONSE DATA {}\n", response.getBody());
+
+                                emitter.onNext(response.getBody());
+                                emitter.onComplete();
+
+                            } catch (final Exception e) {
+                                emitter.onError(e);
+                            }
+                        }),
+                endpoint);
+    }
 }
--- FlySpring/edgechain-app/src/main/java/com/edgechain/lib/retrofit/Llama2Service.java
+++ FlySpring/edgechain-app/src/main/java/com/edgechain/lib/retrofit/Llama2Service.java
@@ -1,14 +1,20 @@
 package com.edgechain.lib.retrofit;
 
+import com.edgechain.lib.endpoint.impl.llm.LLamaQuickstart;
 import com.edgechain.lib.endpoint.impl.llm.Llama2Endpoint;
 import com.edgechain.lib.llama2.response.Llama2ChatCompletionResponse;
+import com.edgechain.lib.request.ArkRequest;
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 import java.util.List;
 
 public interface Llama2Service {
-  @POST(value = "llama2/chat-completion")
+  @POST(value = "llama/chat-completion")
   Single<List<Llama2ChatCompletionResponse>> chatCompletion(@Body Llama2Endpoint llama2Endpoint);
+  @POST(value = "llama/chat-completion")
+  Single<String> llamaCompletion(@Body LLamaQuickstart lLamaQuickstart);
 }
