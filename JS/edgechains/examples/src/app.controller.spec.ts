9094c18b91c2e1b104274c23ea1b1d947fa7a557
sadaf <sadaf20043006@gmail.com>
2023-11-05 02:30:19 +0530

    added: test case for hyde-search, code format, added interface for arkRequest

--- JS/edgechains/examples/src/app.controller.spec.ts
+++ JS/edgechains/examples/src/app.controller.spec.ts
@@ -19,4 +19,31 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+  describe('POST /hyde-search/query-rrf', () => {
+    it('for querying top 5 programming languages the response should contain Java', async () => {
+      const params = {
+        topK: 5,
+      };
+      const query = {
+        metadataTable: 'title_metadata',
+        query: 'tell me the top 5 programming languages currently',
+        textWeight: {
+          baseWeight: '1.0',
+          fineTuneWeight: '0.35',
+        },
+        similarityWeight: {
+          baseWeight: '1.5',
+          fineTuneWeight: '0.40',
+        },
+        dateWeight: {
+          baseWeight: '1.25',
+          fineTuneWeight: '0.75',
+        },
+        orderRRF: 'default',
+      };
+      expect(
+        (await appController.hydeSearch(params, query)).finalAnswer,
+      ).toContain('Java');
+    }, 12000);
+  });
 });
--- JS/edgechains/examples/src/lib/OpenAiEndpoint.ts
+++ JS/edgechains/examples/src/lib/OpenAiEndpoint.ts
@@ -1,112 +1,137 @@
 import axios from 'axios';
 
-export class OpenAiEndpoint{
+export class OpenAiEndpoint {
+  url: string;
+  apiKey: string;
+  orgId: string;
+  model: string;
+  role: string;
+  temprature: number;
 
-    url:string;
-    apiKey:string;
-    orgId:string;
-    model:string;
-    role:string;
-    temprature:number;
+  constructor(
+    url: string,
+    apiKey: string,
+    orgId: string,
+    model: string,
+    role: string,
+    temprature: number,
+  ) {
+    this.url = url;
+    this.apiKey = apiKey;
+    this.orgId = orgId;
+    this.model = model;
+    this.role = role;
+    this.temprature = temprature;
+  }
 
-    constructor(url:string,apiKey:string,orgId:string,model:string,role:string,temprature:number){
-        this.url = url;
-        this.apiKey = apiKey;
-        this.orgId = orgId;
-        this.model = model;
-        this.role = role;
-        this.temprature = temprature;
-    }
-
-    async gptFn(prompt:string): Promise<string>{
-        const responce = await axios.post('https://api.openai.com/v1/chat/completions', {
-      'model' : this.model,
-      'messages' : [{
-        'role' : this.role,
-        'content' : prompt
-      }],
-      'temperature' : this.temprature
-    },{
-      headers : {
-        Authorization : 'Bearer ' + this.apiKey ,
-        'content-type' : 'application/json'
-      }
-    })
-    .then(function(response){
-      return response.data.choices
-    }
-      
-    )
-    .catch(function (error) {
-      if (error.response) {
-        console.log('Server responded with status code:', error.response.status);
-        console.log('Response data:', error.response.data);
-      } else if (error.request) {
-        console.log('No response received:', error.request);
-      } else {
-        console.log('Error creating request:', error.message);
-      }
-    });
-    return responce[0].message.content;
-    }
-
-    async embeddings(resp : string): Promise<Number[]> {
-        const responce = await axios.post('https://api.openai.com/v1/embeddings', {
-          "model" : "text-embedding-ada-002",
-          "input" : resp
-          },{
-            headers : {
-              Authorization : 'Bearer ' + this.apiKey ,
-              'content-type' : 'application/json'
-            }
-          })
-          .then(function(response){
-            return response.data.data[0].embedding;
-          }
-            
-          )
-          .catch(function (error) {
-            if (error.response) {
-              console.log('Server responded with status code:', error.response.status);
-              console.log('Response data:', error.response.data);
-            } else if (error.request) {
-              console.log('No response received:', error.request);
-            } else {
-              console.log('Error creating request:', error.message);
-            }
-          });
-      
-          return responce;
-    }
-
-    async gptFnChat(chatMessages){
-        const responce = await axios.post('https://api.openai.com/v1/chat/completions', {
-            'model' : this.model,
-            'messages' : chatMessages,
-            'temperature' : this.temprature
-        },{
-            headers : {
-            Authorization : 'Bearer ' + this.apiKey ,
-            'content-type' : 'application/json'
-            }
-        })
-        .then(function(response){
-            return response.data.choices
+  async gptFn(prompt: string): Promise<string> {
+    const responce = await axios
+      .post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: this.model,
+          messages: [
+            {
+              role: this.role,
+              content: prompt,
+            },
+          ],
+          temperature: this.temprature,
+        },
+        {
+          headers: {
+            Authorization: 'Bearer ' + this.apiKey,
+            'content-type': 'application/json',
+          },
+        },
+      )
+      .then(function (response) {
+        return response.data.choices;
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(
+            'Server responded with status code:',
+            error.response.status,
+          );
+          console.log('Response data:', error.response.data);
+        } else if (error.request) {
+          console.log('No response received:', error.request);
+        } else {
+          console.log('Error creating request:', error.message);
         }
-            
-        )
-        .catch(function (error) {
-            if (error.response) {
-            console.log('Server responded with status code:', error.response.status);
-            console.log('Response data:', error.response.data);
-            } else if (error.request) {
-            console.log('No response received:', error.request);
-            } else {
-            console.log('Error creating request:', error.message);
-            }
-        });
+      });
+    return responce[0].message.content;
+  }
 
-        return responce[0].message.content;
-    }
+  async embeddings(resp: string): Promise<number[]> {
+    const responce = await axios
+      .post(
+        'https://api.openai.com/v1/embeddings',
+        {
+          model: 'text-embedding-ada-002',
+          input: resp,
+        },
+        {
+          headers: {
+            Authorization: 'Bearer ' + this.apiKey,
+            'content-type': 'application/json',
+          },
+        },
+      )
+      .then(function (response) {
+        return response.data.data[0].embedding;
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(
+            'Server responded with status code:',
+            error.response.status,
+          );
+          console.log('Response data:', error.response.data);
+        } else if (error.request) {
+          console.log('No response received:', error.request);
+        } else {
+          console.log('Error creating request:', error.message);
+        }
+      });
 
+    return responce;
+  }
+
+  async gptFnChat(chatMessages) {
+    const responce = await axios
+      .post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: this.model,
+          messages: chatMessages,
+          temperature: this.temprature,
+        },
+        {
+          headers: {
+            Authorization: 'Bearer ' + this.apiKey,
+            'content-type': 'application/json',
+          },
+        },
+      )
+      .then(function (response) {
+        return response.data.choices;
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(
+            'Server responded with status code:',
+            error.response.status,
+          );
+          console.log('Response data:', error.response.data);
+        } else if (error.request) {
+          console.log('No response received:', error.request);
+        } else {
+          console.log('Error creating request:', error.message);
+        }
+      });
+
+    return responce[0].message.content;
+  }
 }
