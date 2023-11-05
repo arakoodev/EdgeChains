bb6afbeb2fa3ab351d17821a3661b0e905e5e495
sadaf <sadaf20043006@gmail.com>
2023-11-06 00:13:44 +0530

    added: ui for hyde-search

--- JS/edgechains/examples/src/app.controller.ts
+++ JS/edgechains/examples/src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Query,
+  Render,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { hydeSearchAdaEmbedding } from './hydeExample/hydeExample';
 
@@ -13,16 +21,26 @@ export class AppController {
 
   @Post('/hyde-search/query-rrf')
   @HttpCode(200)
-  hydeSearch(@Query() params: any, @Body() query: any) {
+  async hydeSearch(@Query() params: any, @Body() query: any) {
+    const jsonData = JSON.parse(query.jsonData);
     const arkRequest = {
       topK: params.topK,
-      metadataTable: query.metadataTable,
-      query: query.query,
-      textWeight: query.textWeight,
-      similarityWeight: query.similarityWeight,
-      dateWeight: query.dateWeight,
-      orderRRF: query.orderRRF,
+      metadataTable: jsonData.metadataTable,
+      query: jsonData.query,
+      textWeight: jsonData.textWeight,
+      similarityWeight: jsonData.similarityWeight,
+      dateWeight: jsonData.dateWeight,
+      orderRRF: jsonData.orderRRF,
     };
-    return hydeSearchAdaEmbedding(arkRequest);
+    const result = await hydeSearchAdaEmbedding(arkRequest);
+    return result.finalAnswer;
+  }
+
+  @Get('/hyde-search/query-rrf')
+  @Render('hyde-search')
+  root() {
+    return {
+      result: '',
+    };
   }
 }
