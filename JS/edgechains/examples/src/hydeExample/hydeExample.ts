7ce27d2812e21b17847010ebed0a6f5e07a4ca6a
Sadaf <119438857+Sadaf-A@users.noreply.github.com>
2023-11-05 10:09:43 +0530

    Test case for hyde-search (#259)
    * added: test case for hyde-search, code format,
    added interface for arkRequest
    
    * running prettier and linting

--- JS/edgechains/examples/src/hydeExample/hydeExample.ts
+++ JS/edgechains/examples/src/hydeExample/hydeExample.ts
@@ -1,22 +1,24 @@
-import { Jsonnet } from "@hanazuki/node-jsonnet";
+import { Jsonnet } from '@hanazuki/node-jsonnet';
 import * as path from 'path';
-import { OpenAiEndpoint } from 'src/lib/OpenAiEndpoint';
-import { PostgresClient } from 'src/lib/PostgresClient';
-
+import { OpenAiEndpoint } from '../lib/OpenAiEndpoint';
+import { PostgresClient } from '../lib/PostgresClient';
+import { ArkRequest } from '../types/ArkRequest';
 enum PostgresDistanceMetric {
   COSINE = 'COSINE',
   IP = 'IP',
-  L2 = 'L2'
+  L2 = 'L2',
 }
 
-const gpt3endpoint = new OpenAiEndpoint("https://api.openai.com/v1/chat/completions",
-                                        "",
-                                        "",
-                                        "gpt-3.5-turbo",
-                                        "user",
-                                        0.7);
+const gpt3endpoint = new OpenAiEndpoint(
+  'https://api.openai.com/v1/chat/completions',
+  '',
+  '',
+  'gpt-3.5-turbo',
+  'user',
+  0.7,
+);
 
-export async function hydeSearchAdaEmbedding(arkRequest){
+export async function hydeSearchAdaEmbedding(arkRequest: ArkRequest) {
   try {
     // Get required params from API...
     const table = 'ada_hyde_prod';
@@ -27,21 +29,23 @@ export async function hydeSearchAdaEmbedding(arkRequest){
     //
     const jsonnet = new Jsonnet();
 
-    const promptPath = path.join(process.cwd(),'./src/hydeExample/prompts.jsonnet')
-    const hydePath = path.join(process.cwd(),'./src/hydeExample/hyde.jsonnet')
+    const promptPath = path.join(
+      process.cwd(),
+      './src/hydeExample/prompts.jsonnet',
+    );
+    const hydePath = path.join(process.cwd(), './src/hydeExample/hyde.jsonnet');
     // Load Jsonnet to extract args..
-    var promptLoader = await jsonnet
-                            .evaluateFile(promptPath);
+    const promptLoader = await jsonnet.evaluateFile(promptPath);
 
     // Getting ${summary} basePrompt
     const promptTemplate = JSON.parse(promptLoader).summary;
 
     // Getting the updated promptTemplate with query
-    var hydeLoader = await jsonnet
-                        .extString('promptTemplate',promptTemplate)
-                        .extString('time',"")
-                        .extString('query',query)
-                        .evaluateFile(hydePath);
+    let hydeLoader = await jsonnet
+      .extString('promptTemplate', promptTemplate)
+      .extString('time', '')
+      .extString('query', query)
+      .evaluateFile(hydePath);
 
     // Get concatenated prompt
     const prompt = JSON.parse(hydeLoader).prompt;
@@ -53,15 +57,24 @@ export async function hydeSearchAdaEmbedding(arkRequest){
     const gpt3Responses = gptResponse.split('\n');
 
     // Chain 2 ==> Get Embeddings from OpenAI using Each Response
-    const embeddingsListChain: Promise<Number[][]> = Promise.all(
+    const embeddingsListChain: Promise<number[][]> = Promise.all(
       gpt3Responses.map(async (resp) => {
         const embedding = await gpt3endpoint.embeddings(resp);
         return embedding;
-      })
+      }),
     );
 
     // Chain 5 ==> Query via EmbeddingChain
-    const dbClient = new PostgresClient(await embeddingsListChain, PostgresDistanceMetric.IP, topK, 20,table,namespace,arkRequest,15);
+    const dbClient = new PostgresClient(
+      await embeddingsListChain,
+      PostgresDistanceMetric.IP,
+      topK,
+      20,
+      table,
+      namespace,
+      arkRequest,
+      15,
+    );
     const queryResult = await dbClient.dbQuery();
 
     // Chain 6 ==> Create Prompt using Embeddings
@@ -69,10 +82,10 @@ export async function hydeSearchAdaEmbedding(arkRequest){
 
     for (const embeddings of queryResult) {
       retrievedDocs.push(
-        `${embeddings.raw_text}\n score:${embeddings.score}\n filename:${embeddings.filename}\n`
+        `${embeddings.raw_text}\n score:${embeddings.score}\n filename:${embeddings.filename}\n`,
       );
     }
-  
+
     if (retrievedDocs.join('').length > 4096) {
       retrievedDocs.length = 4096;
     }
@@ -81,28 +94,28 @@ export async function hydeSearchAdaEmbedding(arkRequest){
     const formattedTime = currentTime;
 
     // System prompt
-    const ansPromptSystem = JSON.parse(promptLoader).ans_prompt_system 
-    
+    const ansPromptSystem = JSON.parse(promptLoader).ans_prompt_system;
+
     hydeLoader = await jsonnet
-                            .extString(promptTemplate,ansPromptSystem)
-                            .extString('time',formattedTime)
-                            .extString('qeury',retrievedDocs.join(''))
-                            .evaluateFile(hydePath);
+      .extString(promptTemplate, ansPromptSystem)
+      .extString('time', formattedTime)
+      .extString('qeury', retrievedDocs.join(''))
+      .evaluateFile(hydePath);
 
     const finalPromptSystem = JSON.parse(hydeLoader).prompt;
 
     // User prompt
-    const ansPromptUser = JSON.parse(promptLoader).ans_prompt_user
-    
+    const ansPromptUser = JSON.parse(promptLoader).ans_prompt_user;
+
     hydeLoader = await jsonnet
-                            .extString(promptTemplate,ansPromptUser)
-                            .extString('qeury',query)
-                            .evaluateFile(hydePath);
-    const finalPromptUser = JSON.parse(hydeLoader).prompt;;
+      .extString(promptTemplate, ansPromptUser)
+      .extString('qeury', query)
+      .evaluateFile(hydePath);
+    const finalPromptUser = JSON.parse(hydeLoader).prompt;
 
     const chatMessages = [
-      { 'role': 'system', 'content': finalPromptSystem },
-      { 'role': 'user', 'content': finalPromptUser },
+      { role: 'system', content: finalPromptSystem },
+      { role: 'user', content: finalPromptUser },
     ];
 
     const finalAnswer = await gpt3endpoint.gptFnChat(chatMessages);
