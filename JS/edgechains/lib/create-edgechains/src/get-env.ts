c5b78640f084c2ec7062bb65efefb82cb34a0944
sadaf <sadaf20043006@gmail.com>
2023-11-26 04:19:51 +0530

    modified: made the code ready to be published

---
+++ JS/edgechains/lib/create-edgechains/src/get-env.ts
@@ -0,0 +1,10 @@
+function get_env() {
+    let text = `
+    OPENAI_API_KEY=
+    OPENAI_ORG_ID=
+  `.trim();
+  
+    return text.trim() + "\n";
+  }
+  
+  export { get_env };
---
+++ JS/edgechains/lib/create-edgechains/src/get-gitignore.ts
@@ -0,0 +1,16 @@
+function get_gitignore() {
+    let text = `
+  # standard exclusions
+  node_modules
+  
+  # build artifacts
+  dist
+  
+  # environment files
+  .env
+  `.trim();
+  
+    return text.trim() + "\n";
+  }
+  
+  export { get_gitignore };
