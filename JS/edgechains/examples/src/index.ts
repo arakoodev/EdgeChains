877e231cc6e40f07ccc7b0a61a91dc39d54e32af
Sadaf <119438857+Sadaf-A@users.noreply.github.com>
2023-11-26 01:52:30 +0530

    separate html file from controller (#270)
    * code fix
    
    * removed: logs

--- JS/edgechains/examples/src/index.ts
+++ JS/edgechains/examples/src/index.ts
@@ -1,14 +1,17 @@
 import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import DatabaseConnection from "./src/config/db.js";
-import { HydeSearchRouter } from "./src/routes/hydeSearch.route.js";
-
+import DatabaseConnection from "./config/db.js";
+import { HydeSearchRouter } from "./routes/hydeSearch.route.js";
+import { view } from "../htmljs.js"
+import ExampleLayout from "./layouts/ExampleLayout.js";
 DatabaseConnection.establishDatabaseConnection();
 
 const app = new Hono();
 
 app.route("/", HydeSearchRouter);
+
+app.get("/", view(ExampleLayout))
 
 serve(app, () => {
     console.log("server running on port 3000");
