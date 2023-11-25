45d7acb3f39231f176c08b2ab212dcacf8f0f5b9
Sadaf <119438857+Sadaf-A@users.noreply.github.com>
2023-11-25 13:30:28 +0530

    Added build script with esbuild and hyde-search with hono (#266)
    * added: hyde search using hono, htmx and jsx
    
    * added: build script, save commit
    
    * added: build script with esbuild, hyde ui
    
    * resolved: merge conflicts
    
    * modified: added routes following best practices
    
    * added: dotenv for config
    
    * fixed: dependency issues
    
    * added: fragment as a response
    
    * added: eslint, prettier, testcase,
    Service class for Hyde Search, shifting configs to env
    
    * save commit
    
    * save commit
    
    * fixed: folder structure
    
    * fixed: package
    
    * formatted: lib folder

---
+++ JS/edgechains/examples/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import DatabaseConnection from "./src/config/db.js";
+import { HydeSearchRouter } from "./src/routes/hydeSearch.route.js";
+
+DatabaseConnection.establishDatabaseConnection();
+
+const app = new Hono();
+
+app.route("/", HydeSearchRouter);
+
+serve(app, () => {
+  console.log("server running on port 3000");
+});
---
+++ JS/edgechains/examples/src/config/db.ts
@@ -0,0 +1,24 @@
+import {
+  createConnection,
+  Connection,
+  getManager,
+  EntityManager,
+} from "typeorm";
+
+class DatabaseConnection {
+  private static connection: Connection | undefined;
+
+  public static async establishDatabaseConnection(): Promise<Connection> {
+    if (!DatabaseConnection.connection) {
+      DatabaseConnection.connection = await createConnection();
+    }
+    return DatabaseConnection.connection;
+  }
+
+  public static async getEntityManager(): Promise<EntityManager> {
+    await DatabaseConnection.establishDatabaseConnection();
+    return getManager();
+  }
+}
+
+export default DatabaseConnection;
