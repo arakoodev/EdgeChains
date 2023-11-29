ac6904785da7d932d44f88d29409918d59a3a841
Harshvardhan Parmar <harshparmar4902@gmail.com>
2023-11-29 21:27:36 +0530

    Updated OpenAiEndpoint.ts (#276)
    * Automatically applying Prettier changes
    
    * Update OpenAiEndpoint.ts
    
    added method for testcase generation.
    
    * Automatically applying Prettier changes
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- JS/edgechains/lib/src/lib/endpoints/OpenAiEndpoint.ts
+++ JS/edgechains/lib/src/lib/endpoints/OpenAiEndpoint.ts
@@ -125,4 +125,41 @@ export class OpenAiEndpoint {
 
         return responce[0].message.content;
     }
+
+    async gptFnTestGenerator(prompt: string): Promise<string> {
+        const responce = await axios
+            .post(
+                "https://api.openai.com/v1/chat/completions",
+                {
+                    model: this.model,
+                    messages: [
+                        {
+                            role: this.role,
+                            content: prompt,
+                        },
+                    ],
+                    temperature: this.temprature,
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + this.apiKey,
+                        "content-type": "application/json",
+                    },
+                }
+            )
+            .then(function (response) {
+                return response.data.choices;
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    console.log("Server responded with status code:", error.response.status);
+                    console.log("Response data:", error.response.data);
+                } else if (error.request) {
+                    console.log("No response received:", error.request);
+                } else {
+                    console.log("Error creating request:", error.message);
+                }
+            });
+        return responce[0].message.content;
+    }
 }
