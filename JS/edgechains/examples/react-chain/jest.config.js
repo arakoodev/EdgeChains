688bc0b45fdfef6b038e079b394c688053bbdd23
Mohd Saif Khan <119418918+MSaifKhan01@users.noreply.github.com>
2024-01-23 17:14:57 +0530

    React-Chain-Code-Updated and Readme file Added-02 (#310)

---
+++ JS/edgechains/examples/react-chain/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    extensionsToTreatAsEsm: ['.ts'],
+    globals: {
+      'ts-jest': {
+        useESM: true,
+      },
+    },
+  };
+  
--- JS/edgechains/examples/react-chain/src/ReactChain.test.ts
+++ JS/edgechains/examples/react-chain/src/ReactChain.test.ts
@@ -1,13 +1,16 @@
-// const reactChain = require("./ReactChain");
+// Import the reactChainCall function from the ReactChain module
+import { reactChainCall } from "./ReactChain";
 
-import { reactChain } from "./ReactChain";
-
+// Describe block for the ReAct Chain test suite
 describe("ReAct Chain", () => {
+    // Test case: It should return a response
     it("should return a response", async () => {
-        expect(
-            await reactChain(
-                "Author David Chanoff has collaborated with a U.S. Navy admiral who served as the ambassador to the United Kingdom under which President?"
-            )
-        ).toContain("Bill Clinton");
-    }, 30000);
+        // Call the reactChainCall function with a sample question
+        const response = await reactChainCall(
+            "Author David Chanoff has collaborated with a U.S. Navy admiral who served as the ambassador to the United Kingdom under which President?"
+        );
+
+        // Assuming the response is an object with a property 'answer'
+        expect(response).toContain("Bill Clinton");
+    }, 60000); // Increase the timeout to 60 seconds or as needed
 });
