5627efe6b7587a6e3eea0d411051b4cfb8f58545
Shyam Raghuwanshi <sr9993663832@gmail.com>
2024-03-02 22:37:58 +0530

    fix test

--- JS/edgechains/examples/testcases/Palm2AiEndpoints.test.js
+++ JS/edgechains/examples/testcases/Palm2AiEndpoints.test.js
@@ -1,12 +1,10 @@
-const { setTimeout } = require("timers/promises");
 const palm2Prompts = require("./palm2Prompts.json");
 const Palm2ChatFn = require("../../lib/src/lib/endpoints/Palm2AiEndpoint")
-
 
 it('should return a string when called with valid parameters', async () => {
     // Arrange
     const prompt = palm2Prompts["Pm_of_india"];
-    const apiKey = "AIzaSyB2Cc5yX0q5HCOgMWtdsh5mALBorAiLlxE";
+    const apiKey = "1111111111"
     const temperature = 0.1;
 
     // Mock the fetch function
@@ -23,7 +21,7 @@ it('should return a string when called with valid parameters', async () => {
 it('should return a string when called with valid parameters', async () => {
     // Arrange
     const prompt = palm2Prompts["states_in_India"];
-    const apiKey = "AIzaSyB2Cc5yX0q5HCOgMWtdsh5mALBorAiLlxE";
+    const apiKey = "3333333333333";
     const temperature = 0.1;
 
     global.fetch = jest.fn().mockResolvedValue({
@@ -39,7 +37,7 @@ it('should return a string when called with valid parameters', async () => {
 
 it('should return a string when called with valid parameters', async () => {
     const prompt = palm2Prompts["smallest_2_digit_value"];
-    const apiKey = "AIzaSyB2Cc5yX0q5HCOgMWtdsh5mALBorAiLlxE";
+    const apiKey = "333333333334";
     const temperature = 0.1;
 
     global.fetch = jest.fn().mockResolvedValue({
@@ -49,6 +47,8 @@ it('should return a string when called with valid parameters', async () => {
     });
 
     const result = await Palm2ChatFn(prompt, apiKey, temperature);
+
+    console.log(result)
 
     expect(typeof result).toBe("string");
 });
