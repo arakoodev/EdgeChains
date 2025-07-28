5ae22b5e79e7d645bd9258954fcf9ded4916cfe3
Sandeep Srinivasa <sandys@gmail.com>
2025-07-29 00:56:30 +0530

    fix build and add typings

--- ec-2/nexusflow/vitest.config.ts
+++ ec-2/nexusflow/vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     environment: "node",
     testTimeout: 10000,
+    // @ts-expect-error - property not in type but supported by vitest
     threads: false,
     sequence: {
       concurrent: false,
