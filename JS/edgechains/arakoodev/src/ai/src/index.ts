e88893bbfc08c84239cec3aee8c49cb46de5df0d
123a-bcd <hagiadong26@gmail.com>
2026-05-23 00:31:03 +0700

    Add PaLM2 API support

--- JS/edgechains/arakoodev/src/ai/src/index.ts
+++ JS/edgechains/arakoodev/src/ai/src/index.ts
@@ -1,5 +1,16 @@
 export { OpenAI } from "./lib/openai/openai.js";
 export { GeminiAI } from "./lib/gemini/gemini.js";
 export { LlamaAI } from "./lib/llama/llama.js";
+export { Palm2AI } from "./lib/palm2/palm2.js";
+export type {
+  Palm2Model,
+  Palm2SafetyCategory,
+  Palm2SafetyRating,
+  Palm2SafetySetting,
+  Palm2SafetyThreshold,
+  Palm2TextCandidate,
+  Palm2TextOptions,
+  Palm2TextResponse,
+} from "./lib/palm2/palm2.js";
 export { RetellAI } from "./lib/retell-ai/retell.js";
 export { RetellWebClient } from "./lib/retell-ai/retellWebClient.js";
---
+++ JS/edgechains/examples/palm2-chat/src/index.ts
@@ -0,0 +1,36 @@
+import { ArakooServer } from "@arakoodev/edgechains.js/arakooserver";
+import { createSyncRPC } from "@arakoodev/edgechains.js/sync-rpc";
+import Jsonnet from "@arakoodev/jsonnet";
+import fileURLToPath from "file-uri-to-path";
+import path from "path";
+
+const server = new ArakooServer();
+const app = server.createApp();
+const jsonnet = new Jsonnet();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const palm2Call = createSyncRPC(
+  path.join(__dirname, "./lib/generateResponse.cjs"),
+);
+
+app.post("/chat", async (c: any) => {
+  try {
+    const { question } = await c.req.json();
+    const key = JSON.parse(
+      jsonnet.evaluateFile(path.join(__dirname, "../jsonnet/secrets.jsonnet")),
+    ).palm_api_key;
+
+    jsonnet.extString("palm_api_key", key);
+    jsonnet.extString("question", question || "");
+    jsonnet.javascriptCallback("palm2Call", palm2Call);
+
+    const response = jsonnet.evaluateFile(
+      path.join(__dirname, "../jsonnet/main.jsonnet"),
+    );
+    return c.json(JSON.parse(response));
+  } catch (error) {
+    console.log("error occured", error);
+    return c.json({ error: "Unable to generate a PaLM2 response" }, 500);
+  }
+});
+
+server.listen(3000);
