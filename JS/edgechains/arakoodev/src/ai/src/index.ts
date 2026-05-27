636e6e5764909ea1d0d577f80f85c5bb17cb4ea3
nguyenduchuyhd <35052285+nguyenduchuyhd@users.noreply.github.com>
2026-05-27 23:47:42 +0200

    Export AWS Comprehend redactor

--- JS/edgechains/arakoodev/src/ai/src/index.ts
+++ JS/edgechains/arakoodev/src/ai/src/index.ts
@@ -1,5 +1,10 @@
-export { OpenAI } from "./lib/openai/openai.js";
+xport { OpenAI } from "./lib/openai/openai.js";
 export { GeminiAI } from "./lib/gemini/gemini.js";
 export { LlamaAI } from "./lib/llama/llama.js";
 export { RetellAI } from "./lib/retell-ai/retell.js";
 export { RetellWebClient } from "./lib/retell-ai/retellWebClient.js";
+export { AwsComprehendRedactor } from "./lib/aws-comprehend/comprehendRedactor.js";
+export type {
+      AwsComprehendRedactorOptions,
+      ChatInputWithPrompt,
+} from "./lib/aws-comprehend/comprehendRedactor.js";
